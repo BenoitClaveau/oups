fcee31688a4a4d4d34463084ffbbe594d7f251fd
Benoît Claveau <benoit.claveau@izzili.com>
2017-10-19 18:41:35 +0200

    unit test

--- tests/runtime.spec.js
+++ tests/runtime.spec.js
@@ -5,8 +5,44 @@
  */
 "use strict";
 
-const RuntimeError = require('../lib/runtime');
+const { RuntimeError } = require('../index');
+const expect = require('expect.js');
 
 describe("runtime", () => {
+
+    // it("RuntimeError", () => {
+    //     try {
+    //         throw new RuntimeError({ template: "Hello ${data.name}!", data: { name: "Ben"} });
+    //     } 
+    //     catch(error) {
+    //         expect(error.message).to.be("Hello Ben!");
+    //         expect(error.name).to.be("RuntimeError");
+    //     }
+    // });
+
+    // it("toString", () => {
+    //     try {
+    //         throw new RuntimeError({ template: "Hello ${data.user.name}!", data: { user: { name: "Paul" }}});
+    //     } 
+    //     catch(error) {
+    //         console.log(error.toString())
+    //     }
+    // });
+
+    it("toString", () => {
+        try {
+            try {
+                throw new Error("Test")
+            } 
+            catch(error) {
+                throw new RuntimeError({error}); //forward error
+            }
+        }
+        catch(runtimeError) {
+            expect(runtimeError.message).to.be("Test");
+            expect(runtimeError.name).to.be("RuntimeError");
+            console.log(runtimeError.toString())
+        }
+    });
 
 });
